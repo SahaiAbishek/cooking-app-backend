2c7b28cd3607403bdb41680d52519ea814660e36
SahaiAbishek <sahaiabhi@gmail.com>
2018-10-17 19:50:43 -0400

    postgresql connection

---
+++ src/main/java/test/PostgreSQLJDBC.java
@@ -0,0 +1,19 @@
+package test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgreSQLJDBC {
+   public static void main(String args[]) {
+      Connection c = null;
+      try {
+         Class.forName("org.postgresql.Driver");
+         c = DriverManager
+            .getConnection("jdbc:postgresql://10.0.0.47:5432/cooking","abhi", "Abhi2207");
+      } catch (Exception e) {
+         e.printStackTrace();
+         System.err.println(e.getClass().getName()+": "+e.getMessage());
+         System.exit(0);
+      }
+      System.out.println("Opened database successfully");
+   }
+}
